62a68db2f7f8222659efbd994316629bedc0f593
haitiano1 <haihoangrr@gmail.com>
2022-10-12 15:27:48 +0700

    Hai up code phan contact-footer-responsive-darkmode

---
+++ js/mainJS.js
@@ -0,0 +1,10 @@
+document.getElementById("switchButtonLaptop").onclick = function () {
+    document.getElementById("myBody").classList.toggle("dark");
+}
+
+document.getElementById("switchButtonMobile").onclick = function () {
+    document.getElementById("myBody").classList.toggle("dark");
+}
+
+
+
